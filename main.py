7323956d932a3bd2a2a90b00a9e004682d77b22a
rzadvornov <roman.zadvornov1-ext@leaseplan.com>
2025-04-23 11:15:11 +0300

    Small cleanup

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from utils.fileutils import clear_directory_files
 from converter.youtube import download_mp3_from_youtube_urls
 from converter.transcriber import transcribe_audio
 
+def main():
+    input_dir = "input"
+    output_dir = "output"
+
+    download_mp3_from_youtube_urls(input_dir)
+    files_to_process = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if
+                      os.path.isfile(os.path.join(input_dir, f))]
+
+    transcribe_audio(files_to_process, input_dir, output_dir)
+    clear_directory_files(input_dir)
+
 
 if __name__ == '__main__':
-    inputDir = "input"
-    outputDir = "output"
-
-    download_mp3_from_youtube_urls(inputDir)
-    filesToProcess = [os.path.join(inputDir, f) for f in os.listdir(inputDir) if
-             os.path.isfile(os.path.join(inputDir, f))]
-
-    transcribe_audio(filesToProcess, inputDir, outputDir)
-    clear_directory_files(inputDir)
+    main()
